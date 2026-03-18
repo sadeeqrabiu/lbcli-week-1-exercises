a644cc527238d70a837a2221b619a6efd28ae730
Abubakar Sadiq <53898703+sadeeqrabiu@users.noreply.github.com>
2026-03-18 02:24:50 +0100

    fix x3

--- submission/05.sh
+++ submission/05.sh
@@ -1,3 +1,7 @@
 # Check the total amount in the wallet.
 
-bitcoin-cli -regtest getbalance
+# Mine blocks on regtest to fund the wallet (requires 101 blocks for coinbase maturity)
+ADDRESS=$(bitcoin-cli -regtest -rpcwallet=builderswallet getnewaddress)
+bitcoin-cli -regtest generatetoaddress 101 "$ADDRESS" > /dev/null
+
+bitcoin-cli -regtest -rpcwallet=builderswallet getbalance
